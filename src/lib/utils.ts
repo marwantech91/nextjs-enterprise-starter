7cf38fd1869f036c336ef8de0d0bb864c94b558a
Marwan Saleh <marwan.saleh.tech@gmail.com>
2026-03-12 11:40:21 +0300

    Add truncate and debounce utility functions

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -33,3 +33,25 @@ export function sleep(ms: number): Promise<void> {
 export function generateId(): string {
   return Math.random().toString(36).substring(2, 9);
 }
+
+/**
+ * Truncate a string to a maximum length with ellipsis
+ */
+export function truncate(str: string, maxLength: number): string {
+  if (str.length <= maxLength) return str;
+  return str.slice(0, maxLength - 1).trimEnd() + '\u2026';
+}
+
+/**
+ * Debounce a function call
+ */
+export function debounce<T extends (...args: unknown[]) => void>(
+  fn: T,
+  delay: number
+): (...args: Parameters<T>) => void {
+  let timeoutId: ReturnType<typeof setTimeout>;
+  return (...args: Parameters<T>) => {
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(() => fn(...args), delay);
+  };
+}
